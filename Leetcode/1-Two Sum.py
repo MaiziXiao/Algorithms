f6fdef1ad7bddbc82a85c5d13463fff6f21b96bb
MaiziXiao <xxllcc1993@gmail.com>
2019-12-12 16:50:12 +0100

    Add comments and descriptions for problems

--- Leetcode/1-Two Sum.py
+++ Leetcode/1-Two Sum.py
@@ -1,24 +1,37 @@
 import itertools
+
+
 class Solution(object):
+    """
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    Example:
+    Given nums = [2, 7, 11, 15], target = 9,
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
+    """
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-#one pass hash-table
+
+        # 1. one pass hash-table
         # lookup = {}
         # for i, num in enumerate(nums):
         #     if target - num in lookup:
         #         return [lookup[target - num], i]
         #     lookup[num] = i
         # return []
-#brute force
-        #own solution
-        for a,b in itertools.combinations(enumerate(nums), 2):
+
+        # 2. brute force (own solution)
+        #
+        for a, b in itertools.combinations(enumerate(nums), 2):
             if a[1] + b[1] == target:
                 return [a[0], b[0]]
-        #online
+
+        # 3. brute force (own solution)
         # k = 0
         # for i in nums:
         #     j = target - i
@@ -26,7 +39,9 @@ class Solution(object):
         #     tmp_nums = nums[k:]
         #     if j in tmp_nums:
         #         return [k - 1, tmp_nums.index(j) + k]
-nums1 = [3,3]
+
+
+nums1 = [3, 2, 5, 1, 3]
 target1 = 6
 solution = Solution()
 print(solution.twoSum(nums1,target1))
