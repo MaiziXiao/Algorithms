8b6f3857d5ce34e96c43872665c9b91662239428
Linchen Xiao <linchen.xiaometronom.com>
2019-12-20 15:44:23 +0100

    Problem 48 added!

--- Leetcode/数组链表/48-medium-Rotate Image.py
+++ Leetcode/数组链表/48-medium-Rotate Image.py
@@ -31,20 +31,24 @@ class Solution:
         if n == 1:
             return n
         # 圈数
-        for i in range(n//2):
-            for j in range(i):
-                matrix[]
+        for i in range(n//2+1):
+            for j in range(i, n-i-1):
+                tmp = matrix[i][j]
+                matrix[i][j] = matrix[n-j-1][i]
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+                matrix[j][n-i-1] = tmp
+        return matrix
+
         # Online Solution
         # 先做矩阵转置（即沿着对角线翻转），然后每个列表翻转；
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        for m in matrix:
-            m.reverse()
+        # n = len(matrix)
+        # for i in range(n):
+        #     for j in range(i, n):
+        #         matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # for m in matrix:
+        #     m.reverse()
+        # return matrix
 
 
-Solution().rotate([
-          [1,2,3],
-          [4,5,6],
-          [7,8,9]])
+print(Solution().rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
