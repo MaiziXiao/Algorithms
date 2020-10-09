ab1dcfbfca289c1186cf22dceb11208f60410255
Xiao, Linchen <linchen.xiao@metronom.com>
2020-10-09 14:02:19 +0200

    回溯法

--- 剑指offer/13-机器人的运动范围.py
+++ 剑指offer/13-机器人的运动范围.py
@@ -1,4 +1,8 @@
 class Solution:
+    def __init__(self):
+        self.possible = None
+        self.num = 0
+
     def movingCount(self, m: int, n: int, k: int) -> int:
         """
         地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，
@@ -13,4 +17,30 @@ class Solution:
         示例 2：
         输入：m = 3, n = 1, k = 0
         输出：1
+        1 <= n,m <= 100
+        0 <= k <= 20
         """
+
+        def dfs(row, column):
+            if row < 0 or row >= m or column < 0 or column >= n or self.possible[row][column] != 0:
+                return
+            elif (row // 10) % 10 + (row // 1) % 10 + (column // 10) % 10 + (column / 1) % 10 > k:
+                self.possible[row][column] = False
+                return
+            else:
+                self.possible[row][column] = True
+                self.num += 1
+                dfs(row + 1, column)
+                dfs(row - 1, column)
+                dfs(row, column + 1)
+                dfs(row, column - 1)
+                # 这道题的 DFS 没有在递归调用邻居之后将其从 visit 中移除. 这是因为上道题中一个点可能出现在多条路径中,
+                # 所以必须调用完之后从集合移除, 避免之后的路径无法使用该节点; 而这道题只有一个共享的运动范围,
+                # 所以一个点如果出现在运动范围内的话, 那么它一定不需要再次被访问了, 直接永久加入 visit 集合中即可
+
+        self.possible = [[0] * n for _ in range(m)]
+        dfs(0, 0)
+
+        return self.num
+
+Solution().movingCount(1,2,1)
