6484f6bc16353b3270768c528344e3bb56e6f271
MaiziXiao <xxllcc1993@gmail.com>
2020-10-10 17:48:21 +0200

    快慢指针

---
+++ 剑指offer/22-链表中倒数第k个节点.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        """
+        输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+        例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个节点是值为4的节点。
+
+        示例：
+        给定一个链表: 1->2->3->4->5, 和 k = 2.
+        返回链表 4->5.
+        """
+        # 时间复杂度O(n),空间复杂度O(n)
+        # node_list = []
+        # while head:
+        #     node_list.append(head)
+        #     head = head.next
+        # if len(node_list) < k:
+        #     return None
+        # else:
+        #     return node_list[-k]
+
+        # 时间复杂度O(n),空间复杂度O(1)
+        behind_node = head
+        while head:
+            print(head)
+            if k > 0:
+                k -= 1
+            else:
+                behind_node=behind_node.next
+            head = head.next
+        if k == 0:
+            return behind_node
+        else:
+            return None
