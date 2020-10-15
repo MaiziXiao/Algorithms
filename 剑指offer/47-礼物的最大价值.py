a0b0b372b2e080299bd478a99d64d0e139d58d32
MaiziXiao <xxllcc1993@gmail.com>
2020-10-15 21:22:16 +0200

    动态规划

--- 剑指offer/47-礼物的最大价值.py
+++ 剑指offer/47-礼物的最大价值.py
@@ -1,30 +1,34 @@
 from typing import List
-
 class Solution:
     def maxValue(self, grid: List[List[int]]) -> int:
         """
-        在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，
-        并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
-
+        在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。
+        你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。
+        给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
 
         示例 1:
 
         输入:
         [
-        [1,3,1],
-        [1,5,1],
-        [4,2,1]
+          [1,3,1],
+          [1,5,1],
+          [4,2,1]
         ]
         输出: 12
-        解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
         """
+        m = len(grid)
+        n = len(grid[0])
 
-        m,n=len(grid),len(grid[0])
-        dp = [[0]*(n+1) for _ in range(m+1)]
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + grid[i-1][j-1]
-        return dp[m][n]
+        max_grid = [[None]*n for _ in range(m)]
+        max_grid[0][0] = grid[0][0]
+        for i in range(1, m):
+            max_grid[i][0] = max_grid[i-1][0] + grid[i][0]
+        for i in range(1, n):
+            max_grid[0][i] = max_grid[0][i-1] + grid[0][i]
 
+        for i in range(1, m):
+            for j in range(1, n):
+                max_grid[i][j] = max(max_grid[i-1][j], max_grid[i][j-1])+grid[i][j]
+        return max_grid[m-1][n-1]
 
-print(Solution().maxValue([[1,3,1],[1,5,1],[4,2,1]]))
+print(Solution().maxValue([[1,3,1], [1,5,1], [4,2,1]]))
